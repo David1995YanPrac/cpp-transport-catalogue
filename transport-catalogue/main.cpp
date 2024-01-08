38dd304e174bfd0da5eceb2ac6cd7294e0c5826a
David1995YanPrac <david.ayupov@yandex.ru>
2024-01-08 21:21:16 +0500

    Sprint 10

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,35 +1,16 @@
-#include <iostream>
-#include <string>
-
-#include "input_reader.h"
-#include "stat_reader.h"
-
-using namespace std;
-using namespace transport_catalogue;
+#include "json_reader.h"
+#include "request_handler.h"
 
 int main() {
-    TransportCatalogue catalogue;
+    transport_catalogue::TransportCatalogue catalogue;
+    JsonReader json_doc(std::cin);
 
-    int base_request_count;
-    cin >> base_request_count >> ws;
+    json_doc.FillCatalogue(catalogue);
 
-    {
-        InputReader reader;
-        for (int i = 0; i < base_request_count; ++i) {
-            string line;
-            getline(cin, line);
-            reader.ParseLine(line);
-        }
-        reader.ApplyCommands(catalogue);
+    const auto& stat_requests = json_doc.GetStatRequests();
+    const auto& render_settings = json_doc.GetRenderSettings().AsMap();
+    const auto& renderer = json_doc.FillRenderSettings(render_settings);
 
-    }
-
-    int stat_request_count;
-    cin >> stat_request_count >> ws;
-    for (int i = 0; i < stat_request_count; ++i) {
-        string line;
-        getline(cin, line);
-        ParseAndPrintStat(catalogue, line, cout);
-    }
-
+    RequestHandler rh(catalogue, renderer);
+    json_doc.ProcessRequests(stat_requests, rh);
 }
